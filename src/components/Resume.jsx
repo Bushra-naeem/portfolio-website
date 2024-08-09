5030f9341378fdc1af963d1d9598628bf545c68a
Bushra <bushranaeem941@gmail.com>
2024-08-10 00:43:18 +0500

    first commit

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { AiOutlineDownload } from "react-icons/ai";
-import pdf from "../assets/Resume - BushraNaeem.pdf";
+import pdf from "../assets/Resume-BushraNaeem.pdf";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import { Document, Page, pdfjs } from "react-pdf";
